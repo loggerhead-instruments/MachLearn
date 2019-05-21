03d1fa794fd6d9e29cd0dfdaa89769b20d9e1d74
Austin <austin.anderson18@ncf.edu>
2019-05-21 16:40:13 -0400

    Code that splits wav files and csv fix
    This code slices wav files as stated in the audited data csv. If the audio segment is less than 1 second long, it is extended. This code does not address files greater than 1 second.
    
    2018-05-01T051000_0004e9e50005718b_2.0.wav was not found within the folder. It's entries were removed from the csv. Look into finding this file.

---
+++ audio_processing/wav_splitter.py
@@ -0,0 +1,66 @@
+from pydub import AudioSegment
+import pandas as pd
+
+# Path to cleaned csv, wav files, and output sliced wav files
+df_path = r"D:\cleaned_concat_fix.csv"
+wav_path = r"D:\wavs"
+wav_out = r"D:\Sliced_Files"
+
+sounds_df = pd.read_csv(df_path)
+
+
+# Extends length of sounds less than 1 second
+def length_extender(length):
+    total_extend = 1 - length
+    side_extend = total_extend / 2
+    new_start = row[2] - side_extend
+
+    return new_start
+
+
+for row in sounds_df.itertuples():
+
+    # If first row of data, load sound file
+    if row[0] == 0:
+        sound_file = row[6]
+        wavfile = AudioSegment.from_wav(wav_path + "\\" + sound_file)
+
+        if row[4] < 1:
+            start = length_extender(row[4])
+
+            audio_slice = wavfile[start*1000:(start+1)*1000]
+            audio_slice.export(wav_out+"\\" + row[1] + "\\"+row[1] + str(row[0]) + ".wav", format="wav")
+
+        else:
+            audio_slice = wavfile[row[2]*1000:row[3]*1000]
+            audio_slice.export(wav_out+"\\" + row[1] + "\\" + row[1] + str(row[0]) + ".wav", format="wav")
+
+    # Else if this is the same sound file as previous row
+    elif row[6] == sound_file:
+
+        if row[4] < 1:
+            start = length_extender(row[4])
+
+            audio_slice = wavfile[start*1000:(start+1)*1000]
+            audio_slice.export(wav_out+"\\" + row[1] + "\\" + row[1] + str(row[0]) + ".wav", format="wav")
+
+        else:
+            audio_slice = wavfile[row[2]*1000:row[3]*1000]
+            audio_slice.export(wav_out + "\\" + row[1] + "\\"+row[1] + str(row[0]) + ".wav", format="wav")
+
+    # This is a new file compared to previous row
+    else:
+        sound_file = row[6]
+        wavfile = AudioSegment.from_wav(wav_path + "\\" + sound_file)
+
+        if row[4] < 1:
+            start = length_extender(row[4])
+
+            audio_slice = wavfile[start*1000:(start+1)*1000]
+            audio_slice.export(wav_out + "\\" + row[1] + "\\" + row[1] + str(row[0]) + ".wav", format="wav")
+
+        else:
+            audio_slice = wavfile[row[2]*1000:row[3]*1000]
+            audio_slice.export(wav_out+"\\" + row[1] + "\\" + row[1] + str(row[0]) + ".wav", format="wav")
+
+# 2018-05-01T051000_0004e9e50005718b_2.0.wav did not exist in wav folder. Removed from csv!
