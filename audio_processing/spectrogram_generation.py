497fe238cd2c5b87242923d62f13c246a0c053b3
Austin <austin.anderson18@ncf.edu>
2019-05-28 09:43:42 -0400

    Ignores 0 byte files from end of file

--- audio_processing/spectrogram_generation.py
+++ audio_processing/spectrogram_generation.py
@@ -16,15 +16,17 @@ configs['n_fft'] = 512
 configs['samples'] = configs['sampling_rate'] * configs['duration']
 configs['dims'] = (257, 260, 1)
 
-for root, dirs, files in os.walk(r'C:\Users\Austin\Documents\Academics\NCF\Semester_2\Loggerhead\practice\tiny_wavs'):
+for root, dirs, files in os.walk(r'D:\Large_Sliced_Files'):
     for file in files:
         # Gets full path of wav
         wav_path = os.path.join(root, file)
         print(wav_path)
 
-        audio, sfreq = lr.load(wav_path, sr = 44100)
+        # if file size is at least 1 byte (deals with end of file error)
+        if os.path.getsize(wav_path) > 1:
+            audio, sfreq = lr.load(wav_path, sr=44100)
 
-        spectrogram = lr.amplitude_to_db(np.abs(lr.stft(audio, n_fft=configs['n_fft'], hop_length=configs['hop_length'])))
-        spectrogram = spectrogram.astype(np.float32)
+            spectrogram = lr.amplitude_to_db(np.abs(lr.stft(audio, n_fft=configs['n_fft'], hop_length=configs['hop_length'])))
+            spectrogram = spectrogram.astype(np.float32)
 
-        plt.imsave(r'C:\Users\Austin\Documents\Academics\NCF\Semester_2\Loggerhead\practice\spec\\' + file[:-4]+".png", spectrogram)
+            plt.imsave(r'C:\Users\Austin\Documents\Academics\NCF\Semester_2\Loggerhead\practice\large_spec\\' + file[:-4]+".png", spectrogram)
