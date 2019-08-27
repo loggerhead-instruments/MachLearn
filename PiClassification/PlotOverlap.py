7ab7da61e0814a7982727231015ccc0f79b558e8
Path-A <36173484+Path-A@users.noreply.github.com>
2019-08-26 23:01:51 -0400

    Added Live Inference with tflite model

--- PiClassification/PlotOverlap.py
+++ PiClassification/PlotOverlap.py
@@ -4,9 +4,11 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
-import librosa
+import librosa as lr
+import tensorflow as tf
 
-#Framework used from sounddevice documentation at https://python-sounddevice.readthedocs.io/en/0.3.13/examples.html
+
+# Framework used from sounddevice documentation at https://python-sounddevice.readthedocs.io/en/0.3.13/examples.html
 def int_or_str(text):
     """Helper function for argument parsing."""
     try:
@@ -30,6 +32,14 @@ parser.add_argument(
     '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
 args = parser.parse_args()
 
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 try:
 
     if args.list_devices:
@@ -52,7 +62,7 @@ try:
 
 
     data = []
-    
+
     with sd.InputStream(samplerate=args.samplerate, device=args.device, channels=args.channels, callback=callback):
         print('#' * 80)
         print('press Ctrl+C to stop the recording')
@@ -64,15 +74,37 @@ try:
             else:
                 # Grabs one second and turns list into single np array
                 X = np.array(data[:args.samplerate])
-                
-                # Resets data to only contain last 0.5 seconds + overhang from queue
-                data = data[args.samplerate//2:]
 
-                S1 = librosa.feature.melspectrogram(np.squeeze(X), sr=args.samplerate)
-                S1 = 10 * np.log(S1 + 1e-15)
-                plt.imshow(S1)
-                plt.pause(0.05)
-                plt.close("all")
+                # Resets data to only contain last 0.5 seconds + overhang from queue
+                data = data[args.samplerate // 2:]
+
+                spectrogram = lr.amplitude_to_db(np.abs(lr.stft(np.squeeze(X), n_fft=256, hop_length=145)))
+                cutoff_spectrogram = spectrogram[3:, :]
+
+                model_spectrogram = np.expand_dims(cutoff_spectrogram, axis=-1)
+
+                # WxHxC --> 1xWxHxC
+                model_spectrogram = np.expand_dims(model_spectrogram, axis=0)
+
+                # Make prediction
+                interpreter.set_tensor(input_details[0]['index'], model_spectrogram)
+                interpreter.invoke()
+
+                tflite_results = interpreter.get_tensor(output_details[0]['index'])
+                print(tflite_results)
+
+                if np.any(tflite_results > .5):
+                    print(tflite_results)
+                    plt.imshow(cutoff_spectrogram)
+                    plt.show()
+                    plt.pause(0.1)
+                    plt.close("all")
+
+            # S1 = librosa.feature.melspectrogram(np.squeeze(X), sr=args.samplerate)
+            # S1 = 10 * np.log(S1 + 1e-15)
+            # plt.imshow(S1)
+            # plt.pause(0.05)
+            # plt.close("all")
 
 
 except KeyboardInterrupt:
